c4891c15308247f15befb70099b2f211ecfd0dc6
Luca <luca.capitoli@thebusinessgame.it>
2022-12-29 18:20:18 +0100

    added db libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val h2_version: String by project
+val hikaricp_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.0"
     id("io.ktor.plugin") version "2.2.1"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.8.0"
 }
 
 group = "com.example"
@@ -32,6 +35,10 @@ dependencies {
     implementation("io.ktor:ktor-server-tomcat-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    //db
+    implementation("org.hibernate:hibernate-core:6.1.6.Final")
+    implementation("com.h2database:h2:$h2_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
